1f820d84ee5522e2165fe72325673ce8b7eac0e7
Ihar Novik <i.novik@godeltech.com>
2019-10-19 21:11:31 +0300

    add book functionality.

--- src/app/adverts/advert/advert.model.ts
+++ src/app/adverts/advert/advert.model.ts
@@ -1,29 +1,31 @@
 export class AdvertModel {
-  public id: number;
-  public head: string;
-  public content: string;
-  public date: Date;
-  public imageUrl: string;
-  public status: AdvertStatus;
-  public price: number;
+    public id: number;
+    public head: string;
+    public content: string;
+    public date: Date;
+    public imageUrl: string;
+    public status: AdvertStatus;
+    public price: number;
+    public giver: any;
 
 
-  constructor(id: number, head: string, content: string, date: Date, 
-    imageUrl: string, status: AdvertStatus, price: number) {
-    if (id) {
-      this.id = id;
+    constructor(id: number, head: string, content: string, date: Date,
+                imageUrl: string, status: AdvertStatus, price: number, giver: any) {
+        if (id) {
+            this.id = id;
+        }
+
+        this.head = head;
+        this.content = content;
+        this.date = date;
+        this.imageUrl = imageUrl;
+        this.status = status;
+        this.price = price;
+        this.giver = giver;
     }
-
-    this.head = head;
-    this.content = content;
-    this.date = date;
-    this.imageUrl = imageUrl;
-    this.status = status;
-    this.price = price;
-  }
 }
 
 export enum AdvertStatus {
-  FREE, BOOKED, ORDERED
+    FREE, BOOKED, ORDERED
 }
 
